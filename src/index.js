69fa3665d119a6bc168c7fc30f3df8ba809f56e0
Popri01 <lucas.popri@gmail.com>
2024-10-03 16:44:39 -0300

    prueba

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ReviewUsers from "./routes/Users.routes.js";
 const app = express();
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Origin", "https://review-app-teal.vercel.app/"); 
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
